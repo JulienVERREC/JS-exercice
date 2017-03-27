8351bebd4fed72733b9d02abe467bfd38436a26b
JulienVERREC <julien.verrecchia@orange.fr>
2017-03-27 11:41:01 +0200

    accent sur mois d'août et retrait de aout

--- array/app.js
+++ array/app.js
@@ -1,4 +1,4 @@
-var mois = ['janvier','février','mars','avril','mai','juin','juillet','août','septembre','octobre','novembre','décembre'];
+var mois = ['janvier','février','mars','avril','mai','juin','juillet','aout','septembre','octobre','novembre','décembre'];
 //exercice 1
 console.log(mois);
 //exercice 2
@@ -7,3 +7,8 @@ console.log(mois[2]);
 console.log([4]);
 //exercice 4
 console.log(mois[10]);
+//exercice 5
+mois.splice(8,0,"août");
+console.log(mois);
+mois.splice(7,1);
+console.log(mois);
