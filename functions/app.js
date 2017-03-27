fd02dfcdb00f99911497f432b258e9978739aac4
JulienVERREC <julien.verrecchia@orange.fr>
2017-03-27 15:30:06 +0200

    fonction qui prend en parametre deux nombres et qui les compare

--- functions/app.js
+++ functions/app.js
@@ -12,3 +12,17 @@ function returnthis(bar,foo){
 	return bar+foo;
 }
 console.log(returnthis("affiche moi ça!","et dépêche toi!"));
+//exercice 4
+function compar(chiffre1,chiffre2){
+ if(chiffre1>chiffre2){
+ 	return "le premier nombre est plus grand";
+ }
+ else if(chiffre1<chiffre2){
+    return "le premier nombre est plus petit";
+ }
+ else {
+ 	return "les deux nombres sont identiques";
+ }
+
+}
+console.log(compar(4,4));
